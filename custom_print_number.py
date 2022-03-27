574d131e2807da5b6a82726686672cf1b51e4868
crist <cristian.ortiz.1990@gmail.com>
2022-03-26 20:48:05 -0500

    Solving print

--- custom_print_number.py
+++ custom_print_number.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-    if 1 <= n <= 20:
-        list1 = []
-        list1 = list(range(n))
-        print(list1)
-        j = []
-        for i in list1:
-            j.append(i*i)
-        print(j)
+    rango = list(range(1, n+1))
+    if 1<=n<=150
+        for i in rango:
+            print(i, sep='')
